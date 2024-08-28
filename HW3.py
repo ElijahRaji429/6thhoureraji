c910f45d88e701e46402568cdce4822168249b85
elijah raji <er125054@student.wpsok.org>
2024-08-28 14:05:54 -0500

    Added HW3

---
+++ HW3.py
@@ -0,0 +1,23 @@
+#Name: ELijah Raji
+#Class:6th hour
+#Assinment:HW3
+
+print("hellow world")
+
+classlist = ["Eijah","CJ","logan","Colan","kyle"]
+
+classlist.append(input())
+
+print(f"{classlist[3]}")
+
+numlist = [2,4,6,9]
+
+numlist.insert(1,10)
+
+print(numlist)
+
+numlist.sort()
+print(numlist)
+
+sum = numlist[0] + numlist[1] + numlist[2]
+print(sum)
