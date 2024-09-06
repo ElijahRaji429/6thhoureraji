d55f4de2f3e9b104b7a288b285d825870c0b48f8
elijah raji <er125054@student.wpsok.org>
2024-09-06 13:54:51 -0500

    Added playground

---
+++ playground.py
@@ -0,0 +1,35 @@
+#Name: ELijah Raji
+#Class:6th hour
+#Assinment: playground
+
+print("hellow world")
+
+print("my name is elijah")
+name = input("what is your name?")
+print(name)
+print("that is a cool name")
+
+print("wanna see a majic trick")
+print("i can add any 2 integers together")
+x = int(input("why dont u give me a integer"))
+y = int(input("give me another integer"))
+z = x + y
+print(z)
+
+numlist = [12,76,78,54,87]
+print("here is a list of numbers")
+print(numlist)
+print("give me a number so i can add it to the list")
+numlist.append(input())
+print(numlist)
+
+print("now watch me sort and add the first 3 numbers")
+print(numlist)
+sum = numlist[0] + numlist[1] + numlist[2]
+print(sum)
+
+nublist = [23,65,934,3245,345]
+print(nublist)
+sub = numlist[0] + numlist[1] + numlist[0] + numlist[2]
+print(sub)
+
