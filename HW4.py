d141057c71e1f7a741faa7c5e8bf8178de36b852
elijah raji <er125054@student.wpsok.org>
2024-09-12 13:53:59 -0500

    Added playground

---
+++ HW4.py
@@ -0,0 +1,41 @@
+#Name: ELijah Raji
+#Class:6th hour
+#Assinment:HW4
+
+print("hellow world")
+
+namedict = {
+    "name" : "Raji",
+    "grade" : "12th",
+    "class" : ["comp sci","art 2","english"]
+}
+
+print(namedict["class"][0])
+
+namedict["height"] = "6'4"
+
+print(namedict)
+
+namedict.clear()
+
+print(namedict)
+
+sixthhour = {
+    "student1": {
+         "name" : "cj",
+         "grade" : 12,
+         "football" : True,
+    },
+    "student2": {
+        "name": "elijah",
+        "grade": 12,
+        "football": True,
+    },
+    "student3": {
+        "name": "colin",
+        "grade": 12,
+        "football": False,
+    }
+}
+
+print(sixthhour)
