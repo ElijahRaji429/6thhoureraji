2174a1cd55bfcdb7f9f29e65dc772d22ca7a3bc7
elijah raji <er125054@student.wpsok.org>
2024-09-27 13:48:27 -0500

    added hw5

--- hw5.py
+++ hw5.py
@@ -11,3 +11,7 @@ numlist = [2,4,6]
 #3. Create an if statement that prints out whichever of the three numbers is the highest
 if 2 < 4 and 4 < 6:
     print("6 is the greatest number")
+elif 2 < 6 and 6 < 4:
+    print("4 is the greatest number")
+elif 6 < 2 and 4 < 2:
+    print("2 is the greatest number")
