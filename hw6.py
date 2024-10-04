7aa463d51ce24ddeeb5a36829638d8620749e114
elijah raji <er125054@student.wpsok.org>
2024-10-04 13:28:05 -0500

    added hw5

---
+++ hw6.py
@@ -0,0 +1,26 @@
+#Name: ELijah Raji
+#Class:6th hour
+#Assinment:hw6
+
+#1. Print Hello World!
+print("hellow world")
+
+#2. Create a variable named num and assign it a number.
+num = 10
+
+#3. Create a nested if statement follows this structure:
+
+if num % 2==0:
+    if num % 3==0:
+        print(num / 2)
+        print(num / 3)
+    else:
+        print("num is not % by 3")
+        print(num / 2)
+else:
+    if num % 3==0:
+        print("num is not % by 2")
+        print(num / 3)
+    else:
+        print("not % by 2 or 3")
+
