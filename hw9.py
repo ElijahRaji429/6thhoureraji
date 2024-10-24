4cafa11df5b57c3555fb20556addd313048172df
elijah raji <er125054@student.wpsok.org>
2024-10-24 13:42:10 -0500

    added hw8

---
+++ hw9.py
@@ -0,0 +1,18 @@
+#Name: Elijah Raji
+#Class:6th hour
+#Assinment:hw9
+
+#1. Print Hello World!
+print("hellow world")
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+temperature = 22
+if temperature > 20:
+    print("its hot")
+
+elif temperature > 10:
+    print("its mild")
+
+else:
+    print("its mild")
+
+print("thank you")
