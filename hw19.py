90a390f84bf6fa3dc532fba21e32946cc0487a75
elijah raji <er125054@student.wpsok.org>
2025-01-31 13:45:50 -0600

    added hw19

---
+++ hw19.py
@@ -0,0 +1,12 @@
+#Name: Elijah Raji
+#Class:6th hour
+#Assinment:hw19
+
+#1. Import all the functions from the HW15 file.
+from hw15 import hello_world, calculate_average, animal_list, numb_loop
+#2. Call the functions here and run HW19.
+numb_loop()
+animal_list("dog","cat","fish","parrot","monkey")
+calculate_average(3,5,6)
+hello_world()
+#3. Delete all calls from HW15 and run HW19 again.
